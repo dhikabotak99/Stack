2ca8305b60c3525841387b560595a86bab3d0cf6
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-14 18:05:56 +0700

    Making Object Class of Stack

--- src/Stack.java
+++ src/Stack.java
@@ -1,13 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Etem
  */
 public class Stack {
-    
+    private int max; //Ukuran dari array stack private long[] stackArray;
+    private int top; //Stack Paling atas
+    private long stackArray[];
+    //Constructor
+    public Stack(int s){
+        max=s;//Menentukan ukuran array
+        top = -1;
+    }
+    public void push(long j){
+        stackArray[++top]=j;
+    }
+    public long pop(){
+        return stackArray[top--];
+    }
+    public Boolean isEmpty(){
+        return(top==-1);
+    }
 }
